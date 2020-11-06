3f3a7c76acc51cae56e51d78d03e2b51fd878f00
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-11-06 17:56:56 +0100

    Update Monster.h

--- Monster.h
+++ Monster.h
@@ -1,101 +1,55 @@
-#ifndef MONSTER_H
-#define MONSTER_H
+#pragma once
 
 #include <string>
 
 
 class Monster {
 protected:
-  std::string name = "";      ///< Name of the monster.
-  float healthPoints = 0;     ///< Current health of the monster.
-  float damage = 0;           ///< Damage, that the monster deals to its opponents.
-  float attackCooldown = 0;   ///< The time it takes to reload the attack.
-  float cooldownState = 0;    ///< Current state of the attack reloading.
 
-  /**
-   * \brief Function for dealing damage.
-   * \param monster the reference of the Monster type, that the damage is dealt to
-   * \param damage the amount of the damage
-   *
-   * Lowers the amount of health points of the given Monster with the amount of the damage parameter.
-   */
-  void sufferDamage(Monster& monster, const float& damage);
+	std::string name = "";      
+	double healthPoints;    
+	double damage;           
+	double attackCooldown;   
+	double cooldownState;   
 
-  /**
-   * \brief Elapse time.
-   * \param monster the Monster type that the elapsed time is applied to
-   * \param t the time to be elapsed
-   *
-   * Modifies the class variables depending on the value of the t parameter.
-   */
-  void elapseTime(Monster& monster, const float& t);
+	void sufferDamage(Monster& monster, const double& damage);
 
-  /**
-   * \brief Cooldown reset.
-   *
-   * Resets the state of the cooldown.
-   */
-  void resetCooldown();
+
+	void elapseTime(Monster& monster, const double& t);
+
+
+	void resetCooldown();
 
 public:
-  //constructor, destructor
 
-  /// Default constructor.
-  Monster() {}
-  ~Monster() {}
-  /// Constructor with parameters for the class.
-  Monster(const std::string& name, const float& healthPoints, const float& damage, const float& attackCooldown) :
-    name(name), healthPoints(healthPoints), damage(damage), attackCooldown(attackCooldown), cooldownState(attackCooldown) {}
+	Monster() {}
 
-  //functions
 
-  /**
-   * \brief Check if is alive.
-   * \return bool
-   *
-   * Checks if the monster is alive, depending on its health points.
-   */
-  bool isAlive() const { return healthPoints > 0; }
+	Monster(const std::string& name, const double& healthPoints, const double& damage, const double& attackCooldown) :
+		name(name), healthPoints(healthPoints), damage(damage), attackCooldown(attackCooldown), cooldownState(attackCooldown) {}
 
-  /**
-   * \brief Check if able to hit.
-   * \return bool
-   *
-   * Checks if the monster is able to attack, depending on its cooldown state.
-   */
-  bool canHit() const { return cooldownState <= 0; }
 
-  /**
-   * \brief JSON parser.
-   * \param filename string that describes the file to be opened
-   * \return Monster class
-   * \exception JSON::ParseException is thrown on syntax and reading errors, from the JSON class
-   *
-   * Parses a simple JSON file and returns its values as a Monster object.
-   */
-  static Monster parse(const std::string& filename);
+	bool isAlive() const { return healthPoints > 0; }
 
-  /**
-   * \brief Attack function.
-   * \param other a Monster type object
-   *
-   * Performs an attack on the Monster type given as a parameter.
-   */
-  virtual void attack(Monster& other);
 
-  //getters
+	bool canHit() const { return cooldownState <= 0; }
 
-  /// Getter for the name variable.
-  std::string getName() const { return name; }
 
-  /// Getter for the healthPoints variable.
-  float getHealthPoints() const { return healthPoints; }
+	static Monster parse(const std::string& filename);
 
-  /// Getter for the damage variable.
-  float getDamage() const { return damage; }
 
-  /// Getter for the attackCooldown variable.
-  float getAttackCooldown() const { return attackCooldown; }
+	virtual void attack(Monster& enemy);
+
+
+	std::string getName() const { return name; }
+
+
+	double getHealthPoints() const { return healthPoints; }
+
+
+	double getDamage() const { return damage; }
+
+	
+	double getAttackCooldown() const { return attackCooldown; }
 };
 
-#endif
