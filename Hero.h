c3e742616c76002b6cbb40263449d265946a431d
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-11-06 17:56:09 +0100

    Update Hero.h

--- Hero.h
+++ Hero.h
@@ -1,66 +1,44 @@
-#ifndef HERO_H
-#define HERO_H
+#pragma once
 
 #include "Monster.h"
 
 
 class Hero : public Monster {
-  float baseHealthPoints = 0;             ///< Maximum health that the hero can have.
 
-  float experiencePerLevel = 0;           ///< The amount of experience needed for reaching the next level.
-  float experienceState = 0;              ///< The current amount of experience the hero has.
-  float healthPointBonusPerLevel = 0;     ///< The extra health points the hero obtains from leveling up.
-  float damageBonusPerLevel = 0;          ///< The extra damage the hero obtains from leveling up.
-  float cooldownMultiplierPerLevel = 0;   ///< Modifies the attack cooldown when leveling up.
-  float level = 0;                        ///< The actual level of the hero.
+	double baseHealthPoints;            
+	double experiencePerLevel;          
+	double experienceState ;              
+	double healthPointBonusPerLevel;     
+	double damageBonusPerLevel;          
+	double cooldownMultiplierPerLevel;   
+
+	double level;                        
 
 public:
-  //constructor, destructor
 
-  /// Default constructor
-  Hero() {}
-
-  /// Constructor with parameters for the class.
-  Hero(const std::string& name, const float& healthPoints, const float& damage, const float& attackCooldown, const float& experiencePerLevel,
-    const float& healthPointBonusPerLevel, const float& damageBonusPerLevel, const float& cooldownMultiplierPerLevel) :
-    Monster(name, healthPoints, damage, attackCooldown), baseHealthPoints(healthPoints), experiencePerLevel(experiencePerLevel),
-    experienceState(0), healthPointBonusPerLevel(healthPointBonusPerLevel), damageBonusPerLevel(damageBonusPerLevel),
-    cooldownMultiplierPerLevel(cooldownMultiplierPerLevel), level(1) {}
+	Hero() {}
 
 
-  //functions
+	Hero(const std::string& name, const double& healthPoints, const double& damage, const double& attackCooldown, const double& experiencePerLevel,
+		const double& healthPointBonusPerLevel, const double& damageBonusPerLevel, const double& cooldownMultiplierPerLevel) :
+		Monster(name, healthPoints, damage, attackCooldown), baseHealthPoints(healthPoints), experiencePerLevel(experiencePerLevel),
+		experienceState(0), healthPointBonusPerLevel(healthPointBonusPerLevel), damageBonusPerLevel(damageBonusPerLevel),
+		cooldownMultiplierPerLevel(cooldownMultiplierPerLevel), level(1) {}
 
-  /**
-   * \brief JSON parser.
-   * \param filename string that describes the file to be opened
-   * \return Hero class
-   * \exception JSON::ParseException is thrown on syntax and reading errors, from the JSON class
-   *
-   * Parses a simple JSON file and returns its values as a Hero object.
-   */
-  static Hero parse(const std::string& filename);
 
-  /**
-   * \brief Attack function.
-   * \param other a Monster type object
-   *
-   * Performs an attack on the Monster type given as a parameter, overloads the base class's attack function.
-   */
-  void attack(Monster& other);
 
-  /**
-   * \brief Fight with another Monster type.
-   * \param other the other Monster type to fight with
-   *
-   * Simulates a fight between the Hero and an other Monster type object, until one of them loses.
-   */
-  void fightTilDeath(Monster& other);
+	static Hero parse(const std::string& filename);
 
-  /// Getter for the baseHealthPoints variable.
-  float getMaxHealthPoints() const { return baseHealthPoints; }
 
-  /// Getter for the level variable.
-  float getLevel() const { return level; }
+	void attack(Monster& enemy);
+
+
+	void fightTilDeath(Monster& enemy);
+
+	
+	double getMaxHealthPoints() const { return baseHealthPoints; }
+
+	
+	double getLevel() const { return level; }
 };
 
-#endif
