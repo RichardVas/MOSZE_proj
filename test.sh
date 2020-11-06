c5bc699f279bef423885746c5dfaddaf597f6bc4
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-11-06 17:42:44 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,7 @@ IFS=$'\n'
 
 rm -f output.txt
 
-echo `./main scenario1.json` >> $2
-echo `./main scenario2.json` >> $2
+./a.out scenario1.json >> output.txt
+./a.out scenario2.json >> output.txt
+
+
