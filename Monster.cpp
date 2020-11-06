0bed162bfd50d9a01898e1d6a2502c926ddd933c
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-11-06 14:08:01 +0100

    Update and rename Fighter.cpp to Monster.cpp

---
+++ Monster.cpp
@@ -0,0 +1,42 @@
+#include "Monster.h"
+#include "JSON.h"
+
+void Monster::take_dmg(Monster& monster, const double& damage) {
+	monster.healthPoints -= damage;
+	if (monster.healthPoints < 0) { monster.healthPoints = 0; }
+}
+
+//void Monster::elapseTime(Monster& monster, const double& t) {
+//	monster.cooldownState -= t;
+//}
+//
+//void Monster::resetCooldown() {
+//	cooldownState += attackCooldown;
+//	if (cooldownState > attackCooldown) {
+//		cooldownState = attackCooldown;
+//	}
+//}
+
+void Monster::attack(Monster& enemy) {
+	if (isAlive() && canHit()) {
+		resetCooldown();
+		take_dmg(enemy, damage);
+	}
+}
+
+Monster Monster::parse(const std::string& filename) {
+	JSON file = JSON::parseFromFile(filename);
+	return Monster(
+		file.get<std::string>("name"),
+		file.get<int>("health_points"),
+		file.get<int>("damage"),
+		file.get<double>("attack_cooldown")
+	);
+}
+
+void Monster::deal_dmg(Monster& enemy,const double& damage)
+{
+	enemy.take_dmg(*this, damage);
+
+
+}
