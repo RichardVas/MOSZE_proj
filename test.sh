9cd0f387dc5af5c3ba80f37516b7b94e8ab09932
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-11-06 15:25:37 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-rm -f output.txt
+IFS=$'\n'
 
-./a.out units/scenario1.json >>curr_output1.txt
-.a .out units/scenario2.json >>curr_output2.txt
+rm output.txt
 
-diff --strip-trailing-cr curr_output1.txt output1.txt
-diff --strip-trailing-cr curr_output2.txt output2.txt
-
-if [ $? -eq 0 ]
-then
-echo "No differences. The test was OK!"
-else
-echo "Test failed."
-fi
-
+echo `./main scenario1.json` >> $2
+echo `./main scenario2.json` >> $2
