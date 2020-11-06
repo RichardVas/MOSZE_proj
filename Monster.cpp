8108fd6d4efcc3912718395c18a72b375d5b674a
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-11-06 17:31:02 +0100

    Update Monster.cpp

--- Monster.cpp
+++ Monster.cpp
@@ -1,35 +1,35 @@
 #include "Monster.h"
 #include "JSON.h"
 
-void Monster::sufferDamage(Monster& monster, const double& damage) {
-	monster.healthPoints -= damage;
-	if (monster.healthPoints < 0) { monster.healthPoints = 0; }
+void Monster::sufferDamage(Monster& monster, const float& damage) {
+  monster.healthPoints -= damage;
+  if (monster.healthPoints < 0) { monster.healthPoints = 0; }
 }
 
-void Monster::elapseTime(Monster& monster, const double& t) {
-	monster.cooldownState -= t;
+void Monster::elapseTime(Monster& monster, const float& t) {
+  monster.cooldownState -= t;
 }
 
 void Monster::resetCooldown() {
-	cooldownState += attackCooldown;
-	if (cooldownState > attackCooldown) {
-		cooldownState = attackCooldown;
-	}
+  cooldownState += attackCooldown;
+  if (cooldownState > attackCooldown) {
+    cooldownState = attackCooldown;
+  }
 }
 
-void Monster::attack(Monster& enemy) {
-	if (isAlive() && canHit()) {
-		resetCooldown();
-		sufferDamage(enemy, damage);
-	}
+void Monster::attack(Monster& other) {
+  if (isAlive() && canHit()) {
+    resetCooldown();
+    sufferDamage(other, damage);
+  }
 }
 
 Monster Monster::parse(const std::string& filename) {
-	JSON file = JSON::parseFromFile(filename);
-	return Monster(
-		file.get<std::string>("name"),
-		file.get<int>("health_points"),
-		file.get<int>("damage"),
-		file.get<double>("attack_cooldown")
-	);
+  JSON file = JSON::parseFromFile(filename);
+  return Monster(
+    file.get<std::string>("name"),
+    file.get<int>("health_points"),
+    file.get<int>("damage"),
+    file.get<float>("attack_cooldown")
+  );
 }
