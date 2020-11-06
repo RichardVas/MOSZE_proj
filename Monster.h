6093d433d61b36f40ddf48aa93c7ea008b1a5408
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-11-06 14:07:31 +0100

    Update and rename Fighter.h to Monster.h

---
+++ Monster.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <string>
+
+
+class Monster {
+protected:
+
+	std::string name = "";      
+	double healthPoints;    
+	double damage;           
+	double attackCooldown;   
+	double cooldownState;   
+
+	void take_dmg(Monster& enemy, const double& damage);
+	void deal_dmg(Monster& enemy, const double& damage);
+
+	//void elapseTime(Monster& monster, const double& t);
+
+
+	//void resetCooldown();
+
+public:
+
+	Monster() {}
+
+
+	Monster(const std::string& name, const double& healthPoints, const double& damage, const double& attackCooldown) :
+		name(name), healthPoints(healthPoints), damage(damage), attackCooldown(attackCooldown), cooldownState(attackCooldown) {}
+
+
+	bool isAlive() const { return healthPoints > 0; }
+
+
+	bool canHit() const { return cooldownState <= 0; }
+
+
+	static Monster parse(const std::string& filename);
+
+
+	virtual void attack(Monster& enemy);
+
+
+	std::string getName() const { return name; }
+
+
+	double getHealthPoints() const { return healthPoints; }
+
+
+	double getDamage() const { return damage; }
+
+	
+	double getAttackCooldown() const { return attackCooldown; }
+
+	
+};
+
